a14687a4900d5b9ae8465547e2d3afbc6d144ae5
Альбина Рузанова <ruzanovaalbina@gmail.com>
2023-04-05 13:05:47 +0400

    Вывод n числа Фиб. для послед.-ти (1, 1...)

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n) {
+	if (n == 0) return -1;
+	if (n == 1 || n == 2) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
-	cout <<"Hello, World!" << endl;
+	cout << "Hello, World!" << endl;
+	int n;
+	cout << "n = "; cin >> n;
+	cout << "fib(" << n << ") = " << fib(n) << endl;
 
 	return 0;
 }
